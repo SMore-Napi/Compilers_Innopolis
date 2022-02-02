d6b49042650a708fa8bcefe2ab56ddd7c507a6fa
Dablup <danya_livitin@mail.ru>
2022-02-02 20:41:47 +0300

    Deleted all previous stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,5 @@
-import lexical_analysis.Lexer;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String file = "C:\\Users\\dell\\Desktop\\Compilers_Innopolis\\src\\main\\java\\test.f";
-        Path path = Paths.get(file);
-        List<String> lines = Files.readAllLines(path);
+    public static void main(String[] args) {
 
-        Lexer lexer = new Lexer(lines);
-
-
-        //TODO: check if I correctly created all the entities
-
-        //TODO: implement getTokens function
-        lexer.getTokens();
     }
 }
--- src/main/java/lexical_analysis/entities/Literal.java
+++
@@ -1,7 +0,0 @@
-package lexical_analysis.entities;
-
-public class Literal extends Token {
-    public Literal(int length, int row, int column) {
-        super(TokenType.LITERAL, length, row, column);
-    }
-}
