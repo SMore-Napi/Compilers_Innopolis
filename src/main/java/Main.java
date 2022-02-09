fa1bb2918206203000f7fdc69dc458383052170e
SMore-Napi <s.roma245@gmail.com>
2022-02-09 03:18:04 +0300

    Support command line arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,29 @@ import java.io.IOException;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // String programName = "FPrograms/testProgram.txt";
-        String programName = "FPrograms/allTokens.txt";
-        Compiler compiler = new Compiler(programName);
-        List<Token> tokens = compiler.lexicalAnalysis();
-        System.out.println("Tokens found after lexical analysis:");
-        System.out.println();
-        tokens.forEach(System.out::println);
+    public static void main(String[] args) {
+        // String defaultPath = "FPrograms/testProgram.txt";
+        String defaultPath = "FPrograms/allTokens.txt";
+
+        String programSourcePath = args.length >= 1
+                ? programSourcePath = args[0]
+                : defaultPath;
+        String programOutputPath = args.length >= 2
+                ? programOutputPath = args[1]
+                : programSourcePath;
+        programOutputPath = programOutputPath + ".out";
+        run(programSourcePath, programOutputPath);
+    }
+
+    public static void run(String programSourcePath, String programOutputPath) {
+        try {
+            Compiler compiler = new Compiler(programSourcePath);
+            List<Token> tokens = compiler.lexicalAnalysis();
+            System.out.println("Tokens found after lexical analysis:");
+            System.out.println();
+            tokens.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("No such source file: " + programSourcePath);
+        }
     }
 }
